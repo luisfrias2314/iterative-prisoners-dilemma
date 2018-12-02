8492bda38e963dbe99f13d597c6e44eac5b1a16e
luisfrias2313 <42215119+luisfrias2313@users.noreply.github.com>
2018-12-02 00:43:31 -0800

    Revert "team6 comments"
    This reverts commit a10dae857d8b23c482462fae56731a1fdc4f67d9.

--- team6.py
+++ team6.py
@@ -29,28 +29,22 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    def must_common(common=''):
-        '''collude or betray appear most often in oppenents last 5 movies'''
-        
+    def must_common(common=''): 
         y = 0
         x = 0
-        #go through each letter in the last 5 decisions of opponent
         for letters in their_history[-5:]:
-            #the x or y with the highest value is equivalent of the most often letter
             if(letters == 'b'or 'B'):
-                #for every b add one to y
                 y =+ 1
             else:
                 x =+ 1
-        #assign the value of common to the most appeared letter
+
         if (y > x):
             common='b' 
         else:
             common='c' 
-    if len(my_history) < 6: # It's the first six round; collude.
+    if len(my_history) < 6: # It's the first round; collude.
         return 'b'
     else:
-        #returns opposite letter of the most common letter in last 5 decisions
         if must_common(common='b'):
              return 'c'
         else:
